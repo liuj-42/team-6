8cca055ea0ad19a90190663f805b7615911a5bd9
nothingexessive <liuxjames88@gmail.com>
2021-04-13 18:51:29 -0400

    adding stuff to the form
    -added buttons to select from 1-10 for mental wellbeing
    -started the product recommendation section
    -started working on form validation

---
+++ Term Project/mental_health_form.js
@@ -0,0 +1,22 @@
+function focusInitials(formObj) {
+    formObj.initials.focus();
+}
+
+$(function() {
+    $("input").checkboxradio();
+    // $("fieldset").controlgroup();
+});
+
+function validateWellbeing(formObj) {
+    var isValid = true;
+    var error = "Form is valid";
+    
+}
+$(function validateWellBeing(formObj) {
+    var isValid = true;
+    var error = "Form is valid";
+    $("#wellbeing").children().each(function() {
+        var element = $(this).prop('outerHTML');
+    });
+    
+});
