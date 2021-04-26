2eeac773347aef9a38efe741d627d2c876979802
bajajb <bajajb@rpi.edu>
2021-04-26 01:27:08 -0400

    Mental Health Statistics
    Added content to Mental Health Statistics page

--- Term Project/Resources/mental_health_statistics.js
+++ Term Project/Resources/mental_health_statistics.js
@@ -1,18 +1,18 @@
 // Load google charts
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawCharts);
+// Draw Charts
+function drawCharts() {
 
-// Draw the chart and set the chart values
-function drawChart() {
+  // Chart 1
   var data = google.visualization.arrayToDataTable([
     ['Students', 'Percentage'],
     ['', 80],
     ['', 20]
   ]);
 
-  // Chart options
   var options = {
-    title:'Around 80% of students say they deal with stress daily',
+    title:'Around 80% of students say they deal with stress daily.',
     width:550,
     height:400,
     colors: ['#e0440e', '#e6693e'],
@@ -23,6 +23,94 @@ function drawChart() {
     titleTextStyle: {fontSize: 20}
   };
   
-  // Display the chart inside the <div> element with id="piechart"
-  var chart = new google.visualization.PieChart(document.getElementById('chart1'));
-  chart.draw(data, options);
+  var chart1 = new google.visualization.PieChart(document.getElementById('chart1'));
+  chart1.draw(data, options);
+
+  // Chart 2
+  data = google.visualization.arrayToDataTable([
+    ['Students', 'Percentage'],
+    ['', 66],
+    ['', 33]
+  ]);
+
+  options = {
+    title:'Almost two-thirds of students experience severe anxiety.',
+    width:550,
+    height:400,
+    colors: ['#e0440e', '#e6693e'],
+    is3D: true,
+    backgroundColor: '#ea9999',
+    fontName: 'Roboto',
+    legend: {position: 'none'},
+    titleTextStyle: {fontSize: 20}
+  };
+  
+  var chart2 = new google.visualization.PieChart(document.getElementById('chart2'));
+  chart2.draw(data, options);
+
+  // Chart 3
+  data = google.visualization.arrayToDataTable([
+    ['Students', 'Percentage'],
+    ['', 60],
+    ['', 40]
+  ]);
+
+  options = {
+    title:'Over 60% of students drop out of college due to mental health reasons.',
+    width:550,
+    height:400,
+    colors: ['#e0440e', '#e6693e'],
+    is3D: true,
+    backgroundColor: '#ea9999',
+    fontName: 'Roboto',
+    legend: {position: 'none'},
+    titleTextStyle: {fontSize: 20}
+  };
+  
+  var chart3 = new google.visualization.PieChart(document.getElementById('chart3'));
+  chart3.draw(data, options);
+
+  // Chart 4
+  data = google.visualization.arrayToDataTable([
+    ['Students', 'Percentage'],
+    ['', 50],
+    ['', 50]
+  ]);
+
+  options = {
+    title:'Around 50% of students say they have had suicidal thoughts while in college.',
+    width:550,
+    height:400,
+    colors: ['#e0440e', '#e6693e'],
+    is3D: true,
+    backgroundColor: '#ea9999',
+    fontName: 'Roboto',
+    legend: {position: 'none'},
+    titleTextStyle: {fontSize: 20}
+  };
+  
+  var chart4 = new google.visualization.PieChart(document.getElementById('chart4'));
+  chart4.draw(data, options);
+
+  // Chart 5
+  data = google.visualization.arrayToDataTable([
+    ['Students', 'Percentage'],
+    ['', 66],
+    ['', 33]
+  ]);
+
+  options = {
+    title:'Over two-thirds of students say they would use mental health services if they could access them at school.',
+    width:550,
+    height:400,
+    colors: ['#e0440e', '#e6693e'],
+    is3D: true,
+    backgroundColor: '#ea9999',
+    fontName: 'Roboto',
+    legend: {position: 'none'},
+    titleTextStyle: {fontSize: 20}
+  };
+  
+  var chart5 = new google.visualization.PieChart(document.getElementById('chart5'));
+  chart5.draw(data, options);
+}
